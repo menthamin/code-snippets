b17c8037d40c3ed23cb77796a5493406c47b152f
menthamin <menthamin@naver.com>
2022-12-20 22:09:45 +0900

    feat: add javascript concat example

---
+++ javascript/concat.js
@@ -0,0 +1,10 @@
+// https://tutorialpost.apptilus.com/code/posts/js/js07-js-string-number/
+
+conversionId = "AW-123456789";
+conversionLable = "FE-7CMLnyYQYEIXefje5";
+conversionIdLabel = conversionId.concat("/", conversionLable);
+// conversionIdLabel_2 = concat(conversionId, conversionLable); Error
+
+console.log(conversionId, conversionLable);
+console.log(conversionIdLabel);
+// console.log(conversionIdLabel_2);
